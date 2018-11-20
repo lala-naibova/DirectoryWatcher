cfa71ab337f3a0c10c5e3630eb4b282b564ae879
lala-naibova <lala.naibova@gmail.com>
2018-11-20 23:14:27 +0400

    init commit

---
+++ DirectoryWatcher/DirectoryWatcher.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace DirectoryWatcher
+{
+
+    public class DirectoryWatcher
+    {
+
+        private string _path;
+        public TimeSpan Interval { get; set; }
+        public bool IsFileNameSensitive { get; set; }
+
+        public DirectoryWatcher(string path, TimeSpan interval)
+        {
+            this.Interval = interval;
+            this._path = path;
+        }
+
+        public void Start()
+        {
+            string[] oldFiles = Directory.GetFiles(this._path);
+
+            while (true)
+            {
+                Thread.Sleep(this.Interval);
+                string[] currentFiles = Directory.GetFiles(this._path);
+
+                foreach (string currentFile in currentFiles)
+                {
+                    if (!oldFiles.Contains(currentFile))
+                    {
+                        Console.WriteLine($"new file {currentFile}");
+                    }
+                }
+                oldFiles = currentFiles;
+            }
+            
+        }
+    }
+
+}

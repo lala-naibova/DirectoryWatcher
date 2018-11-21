13eecde402dc2cd7f1aec8a52d556e1870ca4c0d
lala-naibova <lala.naibova@gmail.com>
2018-11-21 22:20:24 +0400

    adding new project

---
+++ Runner/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using FileUtils;
+namespace Runner
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] old = {"a","b","c","d","e" };
+            string[] newLetters = {"z","d","j","m" };
+
+            List<string> list = DirectoryWatcher.FindTheNewestFiles(old, newLetters);
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
